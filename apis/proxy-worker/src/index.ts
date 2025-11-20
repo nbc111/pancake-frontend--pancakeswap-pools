9c28717689de953a467053a5f254763f2bf98fb0
chujie <79133@qq.com>
2025-11-20 22:52:18 +0800

    系统浏览器跳转到MetaMask的自带浏览器

--- apis/proxy-worker/src/index.ts
+++ apis/proxy-worker/src/index.ts
@@ -18,7 +18,7 @@ function createEndpoint(url: string) {
     const response = await fetch(url, {
       headers: {
         'X-Forwarded-For': ip,
-        origin: isLocalHost ? 'https://pancakeswap.finance' : headers.get('origin') || '',
+        origin: isLocalHost ? 'https://staking.nbblocks.cc' : headers.get('origin') || '',
       },
       body,
       method: 'POST',
